1808e2a3330625df527cac6a35332e93e448f117
Omar Orozco <ing.omar.orozco@gmail.com>
2018-09-24 18:25:50 -0400

    modificaciones

--- osta/css/themes/theme-choice.php
+++
@@ -1,9 +0,0 @@
-<?php
-$action = $_GET["action"];
-$myText = $_POST["mytheme"];
-
-if($action = "save") {
-  $targetFolder = "";
-  file_put_contents($targetFolder."selected.txt", $myText);
-}
-?>   
