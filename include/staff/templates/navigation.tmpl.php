4e6980633a40629054e92444fec9c27d38f42457
Omar Orozco <ing.omar.orozco@gmail.com>
2018-09-30 11:45:21 -0400

    box shadow

--- include/staff/templates/navigation.tmpl.php
+++ include/staff/templates/navigation.tmpl.php
@@ -3,12 +3,12 @@ if(($tabs=$nav->getTabs()) && is_array($tabs)){
     foreach($tabs as $name =>$tab) {
         if ($tab['href'][0] != '/')
             $tab['href'] = ROOT_PATH . 'scp/' . $tab['href'];
-        echo sprintf('<li class="%s %s"><a href="%s">%s</a>',
+        echo sprintf('<li class="%s %s" style="color:#212331;"><a href="%s">%s</a>',
             $tab['active'] ? 'active':'inactive',
             @$tab['class'] ?: '',
             $tab['href'],$tab['desc']);
         if(!$tab['active'] && ($subnav=$nav->getSubMenu($name))){
-            echo "<ul>\n";
+            echo "<ul style=\"color:#212331;\">\n";
             foreach($subnav as $k => $item) {
                 if (!($id=$item['id']))
                     $id="nav$k";
@@ -16,7 +16,7 @@ if(($tabs=$nav->getTabs()) && is_array($tabs)){
                     $item['href'] = ROOT_PATH . 'scp/' . $item['href'];
 
                 echo sprintf(
-                    '<li><a class="%s" href="%s" title="%s" id="%s">%s</a></li>',
+                    '<li style="color:#212331;"><a class="%s" href="%s" title="%s" id="%s">%s</a></li>',
                     $item['iconclass'],
                     $item['href'], $item['title'],
                     $id, $item['desc']);
