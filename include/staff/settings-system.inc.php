47327bd43dbeefb414e4b3896f5941b9ba732e95
Omar Orozco <ing.omar.orozco@gmail.com>
2018-10-05 13:12:21 -0400

    corrector

--- include/staff/settings-system.inc.php
+++ include/staff/settings-system.inc.php
@@ -70,7 +70,7 @@ $gmtime = Misc::gmtime();
             <td>
                 <input type="text" name="autolock_minutes" size=4 value="<?php echo $config['autolock_minutes']; ?>">
                 <font class="error"><?php echo $errors['autolock_minutes']; ?></font>&nbsp;<?php echo __('minutes'); ?>
-                &nbsp;<i class="help-tip icon-question-sign" href="#collision_avoidance"></i>
+                <!-- &nbsp;<i class="help-tip icon-question-sign" href="#collision_avoidance"></i> -->
             </td>
         </tr>
         <tr><td><?php echo __('Default Page Size');?>:</td>
