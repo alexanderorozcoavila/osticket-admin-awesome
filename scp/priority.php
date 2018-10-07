d8fdf84264fdf742d74dfc5a56658e56637f2e95
Omar Orozco <ing.omar.orozco@gmail.com>
2018-10-07 07:18:18 -0400

    modificacion prefijo tabla prioridad

--- scp/priority.php
+++ scp/priority.php
@@ -14,7 +14,7 @@ require_once(INCLUDE_DIR.'class.export.php');       // For paper sizes
 
 if ( isset( $_REQUEST['ticketid']) != '' ) {
  
-    $sql = "UPDATE ost_ticket__cdata SET priority = '".$_REQUEST['priority']."' WHERE ticket_id = '".$_REQUEST['ticketid']."'";    
+    $sql = "UPDATE os_ticket__cdata SET priority = '".$_REQUEST['priority']."' WHERE ticket_id = '".$_REQUEST['ticketid']."'";    
     echo $sql;
     if ( db_query($sql)) 
         echo $sql;
