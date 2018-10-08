45a07c4816542eadf06cb85ba455662ae60c61c2
Omar Orozco <ing.omar.orozco@gmail.com>
2018-10-08 13:45:19 -0400

    idioma

---
+++ include/i18n/es_ES/MANIFEST.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'Build-Date' => 'Fri, 13 Jan 17 17:13:33 +0000',
+  'Phrases-Version' => '1.10',
+  'Build-Version' => 'v1.10-6-gc0ab967',
+  'Build-Major-Version' => '1.10',
+  'Language' => 'es_ES',
+  'Id' => 'lang:es_ES',
+  'Last-Revision' => '2017-01-13 11:05-0500',
+  'Version' => 148432,
+);
