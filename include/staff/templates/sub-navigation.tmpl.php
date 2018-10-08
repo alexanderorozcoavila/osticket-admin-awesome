6a7b313dd3ce6fcc49c437ecf1af611fcea5fcfc
Omar Orozco <ing.omar.orozco@gmail.com>
2018-10-08 14:02:35 -0400

    idioma

--- include/staff/templates/sub-navigation.tmpl.php
+++ include/staff/templates/sub-navigation.tmpl.php
@@ -23,7 +23,13 @@ if(($subnav=$nav->getSubMenu()) && is_array($subnav)){
             foreach ($item['attr'] as $name => $value)
                 $attr.=  sprintf("%s='%s' ", $name, $value);
 
-        echo sprintf('<li><a class="%s" 33 href="%s" title="%s" id="%s" %s>%s</a></li>',
-                $class, $item['href'], $item['title'], $id, $attr, $item['desc']);
+        $submenu = explode(" ",$item['desc']);
+        $menuTexto = "";
+        if($submenu[0] == "Abrir"){
+            $menuTexto = "Abierto";
+        }
+
+        echo sprintf('<li><a  %s class="%s" 33 href="%s" title="%s" id="%s" %s>%s</a></li>',
+            $menuTexto, $class, $item['href'], $item['title'], $id, $attr, $item['desc']);
     }
 }
